3234ef443dee383784eaa0047927dc5c65d80d44
Albert Jiang <albert594250@gmail.com>
2021-11-22 09:18:56 +0800

    added script for generation with 8 chips

---
+++ int_reproduction/generate_int_script.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+TPU_VISIBLE_CHIPS=0 TPU_CHIPS_PER_HOST_BOUNDS=1,1,1 python3 t5_generation.py --model-path=/home/qj213/INT/data/hf_model \
+    --eval-path=/home/qj213/hf_seq2seq/int_reproduction/int_data/part_1_test.json \
+    --dump-path=/home/qj213/hf_seq2seq/int_reproduction/int_data &
+TPU_VISIBLE_CHIPS=0 TPU_CHIPS_PER_HOST_BOUNDS=1,1,1 python3 t5_generation.py --model-path=/home/qj213/INT/data/hf_model \
+    --eval-path=/home/qj213/hf_seq2seq/int_reproduction/int_data/part_2_test.json \
+    --dump-path=/home/qj213/hf_seq2seq/int_reproduction/int_data &
+TPU_VISIBLE_CHIPS=0 TPU_CHIPS_PER_HOST_BOUNDS=1,1,1 python3 t5_generation.py --model-path=/home/qj213/INT/data/hf_model \
+    --eval-path=/home/qj213/hf_seq2seq/int_reproduction/int_data/part_3_test.json \
+    --dump-path=/home/qj213/hf_seq2seq/int_reproduction/int_data &
+TPU_VISIBLE_CHIPS=0 TPU_CHIPS_PER_HOST_BOUNDS=1,1,1 python3 t5_generation.py --model-path=/home/qj213/INT/data/hf_model \
+    --eval-path=/home/qj213/hf_seq2seq/int_reproduction/int_data/part_4_test.json \
+    --dump-path=/home/qj213/hf_seq2seq/int_reproduction/int_data &
+TPU_VISIBLE_CHIPS=0 TPU_CHIPS_PER_HOST_BOUNDS=1,1,1 python3 t5_generation.py --model-path=/home/qj213/INT/data/hf_model \
+    --eval-path=/home/qj213/hf_seq2seq/int_reproduction/int_data/part_5_test.json \
+    --dump-path=/home/qj213/hf_seq2seq/int_reproduction/int_data &
+TPU_VISIBLE_CHIPS=0 TPU_CHIPS_PER_HOST_BOUNDS=1,1,1 python3 t5_generation.py --model-path=/home/qj213/INT/data/hf_model \
+    --eval-path=/home/qj213/hf_seq2seq/int_reproduction/int_data/part_6_test.json \
+    --dump-path=/home/qj213/hf_seq2seq/int_reproduction/int_data &
+TPU_VISIBLE_CHIPS=0 TPU_CHIPS_PER_HOST_BOUNDS=1,1,1 python3 t5_generation.py --model-path=/home/qj213/INT/data/hf_model \
+    --eval-path=/home/qj213/hf_seq2seq/int_reproduction/int_data/part_7_test.json \
+    --dump-path=/home/qj213/hf_seq2seq/int_reproduction/int_data &
+TPU_VISIBLE_CHIPS=0 TPU_CHIPS_PER_HOST_BOUNDS=1,1,1 python3 t5_generation.py --model-path=/home/qj213/INT/data/hf_model \
+    --eval-path=/home/qj213/hf_seq2seq/int_reproduction/int_data/part_8_test.json \
+    --dump-path=/home/qj213/hf_seq2seq/int_reproduction/int_data &
