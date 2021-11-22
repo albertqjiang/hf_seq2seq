009690017db79e11801c97fc934f367f840c817c
Albert Jiang <albert594250@gmail.com>
2021-11-22 09:27:15 +0800

    minor debug

--- int_reproduction/generate_int_script.sh
+++ int_reproduction/generate_int_script.sh
@@ -3,24 +3,24 @@
 TPU_VISIBLE_CHIPS=0 TPU_CHIPS_PER_HOST_BOUNDS=1,1,1 python3 t5_generation.py --model-path=/home/qj213/INT/data/hf_model \
     --eval-path=/home/qj213/hf_seq2seq/int_reproduction/int_data/part_1_test.json \
     --dump-path=/home/qj213/hf_seq2seq/int_reproduction/int_data &
-TPU_VISIBLE_CHIPS=0 TPU_CHIPS_PER_HOST_BOUNDS=1,1,1 python3 t5_generation.py --model-path=/home/qj213/INT/data/hf_model \
+TPU_VISIBLE_CHIPS=1 TPU_CHIPS_PER_HOST_BOUNDS=1,1,1 python3 t5_generation.py --model-path=/home/qj213/INT/data/hf_model \
     --eval-path=/home/qj213/hf_seq2seq/int_reproduction/int_data/part_2_test.json \
     --dump-path=/home/qj213/hf_seq2seq/int_reproduction/int_data &
-TPU_VISIBLE_CHIPS=0 TPU_CHIPS_PER_HOST_BOUNDS=1,1,1 python3 t5_generation.py --model-path=/home/qj213/INT/data/hf_model \
+TPU_VISIBLE_CHIPS=2 TPU_CHIPS_PER_HOST_BOUNDS=1,1,1 python3 t5_generation.py --model-path=/home/qj213/INT/data/hf_model \
     --eval-path=/home/qj213/hf_seq2seq/int_reproduction/int_data/part_3_test.json \
     --dump-path=/home/qj213/hf_seq2seq/int_reproduction/int_data &
-TPU_VISIBLE_CHIPS=0 TPU_CHIPS_PER_HOST_BOUNDS=1,1,1 python3 t5_generation.py --model-path=/home/qj213/INT/data/hf_model \
+TPU_VISIBLE_CHIPS=3 TPU_CHIPS_PER_HOST_BOUNDS=1,1,1 python3 t5_generation.py --model-path=/home/qj213/INT/data/hf_model \
     --eval-path=/home/qj213/hf_seq2seq/int_reproduction/int_data/part_4_test.json \
     --dump-path=/home/qj213/hf_seq2seq/int_reproduction/int_data &
-TPU_VISIBLE_CHIPS=0 TPU_CHIPS_PER_HOST_BOUNDS=1,1,1 python3 t5_generation.py --model-path=/home/qj213/INT/data/hf_model \
+TPU_VISIBLE_CHIPS=4 TPU_CHIPS_PER_HOST_BOUNDS=1,1,1 python3 t5_generation.py --model-path=/home/qj213/INT/data/hf_model \
     --eval-path=/home/qj213/hf_seq2seq/int_reproduction/int_data/part_5_test.json \
     --dump-path=/home/qj213/hf_seq2seq/int_reproduction/int_data &
-TPU_VISIBLE_CHIPS=0 TPU_CHIPS_PER_HOST_BOUNDS=1,1,1 python3 t5_generation.py --model-path=/home/qj213/INT/data/hf_model \
+TPU_VISIBLE_CHIPS=5 TPU_CHIPS_PER_HOST_BOUNDS=1,1,1 python3 t5_generation.py --model-path=/home/qj213/INT/data/hf_model \
     --eval-path=/home/qj213/hf_seq2seq/int_reproduction/int_data/part_6_test.json \
     --dump-path=/home/qj213/hf_seq2seq/int_reproduction/int_data &
-TPU_VISIBLE_CHIPS=0 TPU_CHIPS_PER_HOST_BOUNDS=1,1,1 python3 t5_generation.py --model-path=/home/qj213/INT/data/hf_model \
+TPU_VISIBLE_CHIPS=6 TPU_CHIPS_PER_HOST_BOUNDS=1,1,1 python3 t5_generation.py --model-path=/home/qj213/INT/data/hf_model \
     --eval-path=/home/qj213/hf_seq2seq/int_reproduction/int_data/part_7_test.json \
     --dump-path=/home/qj213/hf_seq2seq/int_reproduction/int_data &
-TPU_VISIBLE_CHIPS=0 TPU_CHIPS_PER_HOST_BOUNDS=1,1,1 python3 t5_generation.py --model-path=/home/qj213/INT/data/hf_model \
+TPU_VISIBLE_CHIPS=7 TPU_CHIPS_PER_HOST_BOUNDS=1,1,1 python3 t5_generation.py --model-path=/home/qj213/INT/data/hf_model \
     --eval-path=/home/qj213/hf_seq2seq/int_reproduction/int_data/part_8_test.json \
     --dump-path=/home/qj213/hf_seq2seq/int_reproduction/int_data &
