e58664d7f5d31494719faa697d00a7dc6261f0a0
Albert Jiang <qj213@cam.ac.uk>
2021-12-22 09:46:39 +0800

    temper

---
+++ temper_t5.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+str="1"
+while (( ${#str2} < 300 ))
+do
+    str2="${str2}${str}"
+    curl --header "Content-Type: application/json" --request POST --data '{"context": '\""${str2}"\"', "n": 8}' http://localhost:5000/complete
+done
