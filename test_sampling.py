ce7c258b0eb153efef4637f9772eeb27154fd108
Albert Jiang <qj213@cam.ac.uk>
2022-01-03 21:29:25 +0800

    debug

--- test_sampling.py
+++ test_sampling.py
@@ -6,5 +6,5 @@ if __name__ == "__main__":
     tokenizer = T5TokenizerFast.from_pretrained("t5-small")
     input_ids = tokenizer("summarize: hello", return_tensors='jax', padding="max_length", truncation=True).input_ids
     fast_gen = jit(model.generate)
-    summary_ids = fast_gen(input_ids, sampling=True).sequences
+    summary_ids = fast_gen(input_ids, do_sample=True).sequences
     print(tokenizer.decode(summary_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False))
