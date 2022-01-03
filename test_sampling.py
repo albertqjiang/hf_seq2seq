d5d194f40e865afa70731e26f1e16477bb038992
Albert Jiang <qj213@cam.ac.uk>
2022-01-03 21:32:40 +0800

    debug

--- test_sampling.py
+++ test_sampling.py
@@ -1,10 +1,17 @@
 from transformers import FlaxT5ForConditionalGeneration, T5TokenizerFast
 from jax import jit
 
+
+
+
 if __name__ == "__main__":
     model = FlaxT5ForConditionalGeneration.from_pretrained("t5-small")
     tokenizer = T5TokenizerFast.from_pretrained("t5-small")
     input_ids = tokenizer("summarize: hello", return_tensors='jax', padding="max_length", truncation=True).input_ids
-    fast_gen = jit(model.generate)
-    summary_ids = fast_gen(input_ids, do_sample=True).sequences
+
+    def sample(input_ids):
+        return model.generate(input_ids, do_sample=True)
+
+    fast_gen = jit(sample)
+    summary_ids = fast_gen(input_ids).sequences
     print(tokenizer.decode(summary_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False))
