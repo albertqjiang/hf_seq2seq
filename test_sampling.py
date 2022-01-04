3a7efcf87cc59e5f19f545ca730e8c70125bdc01
Albert Jiang <qj213@cam.ac.uk>
2022-01-04 10:40:02 +0800

    debug

--- test_sampling.py
+++ test_sampling.py
@@ -8,9 +8,10 @@ config_path = "aqj213/t5-small-pisa-state-only-finetuned"
 if __name__ == "__main__":
     model = FlaxT5ForConditionalGeneration.from_pretrained(config_path)
     tokenizer = T5TokenizerFast.from_pretrained(config_path)
-    input_ids = tokenizer("summarize: proof (prove) goal: No subgoals!", return_tensors='jax', max_lenght=512, padding="max_length", truncation=True).input_ids
+    input_ids = tokenizer("summarize: proof (prove) goal: No subgoals!", return_tensors='jax', padding="max_length", truncation=True).input_ids
     non_zero = jnp.count_nonzero(input_ids)
-    attention_mask = jnp.pad(jnp.ones(non_zero), (0, 512-non_zero))
+    attention_mask = jnp.zeros_like(input_ids)
+    attention_mask = jnp.place(attention_mask, attention_mask<non_zero, [1.])
     attention_mask = jnp.expand_dims(attention_mask, axis=0)
     
     input_ids = jnp.repeat(input_ids, 8, axis=0)
@@ -24,4 +25,4 @@ if __name__ == "__main__":
     fast_gen = jit(sample)
     summary_ids = fast_gen(input_ids, attention_mask).sequences
     for summary_id in summary_ids:
-        print(tokenizer.decode([summary_id], skip_special_tokens=True, clean_up_tokenization_spaces=False))
+        print(tokenizer.decode(summary_id, skip_special_tokens=True, clean_up_tokenization_spaces=False))
