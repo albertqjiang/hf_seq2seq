96581fd6eefa2a76a16e0b18202fb714caee960b
Albert Jiang <qj213@cam.ac.uk>
2022-01-04 10:55:04 +0800

    debug

--- test_sampling.py
+++ test_sampling.py
@@ -9,18 +9,18 @@ config_path = "aqj213/t5-small-pisa-state-only-finetuned"
 if __name__ == "__main__":
     model = FlaxT5ForConditionalGeneration.from_pretrained(config_path)
     tokenizer = T5TokenizerFast.from_pretrained(config_path)
-    input_ids = tokenizer("summarize: proof (prove) goal: No subgoals!", return_tensors='jax', padding="max_length", truncation=True).input_ids
-    non_zero = jnp.count_nonzero(input_ids)
-    attention_mask = jnp.zeros_like(input_ids)
+    input_ids = tokenizer("summarize: proof (prove) goal: No subgoals!", return_tensors='np', padding="max_length", truncation=True).input_ids
+    non_zero = np.count_nonzero(input_ids)
+    attention_mask = np.zeros_like(input_ids)
     attention_mask = np.place(attention_mask, attention_mask<non_zero, [1.])
-    attention_mask = jnp.expand_dims(attention_mask, axis=0)
+    attention_mask = np.expand_dims(attention_mask, axis=0)
     
-    input_ids = jnp.repeat(input_ids, 8, axis=0)
-    attention_mask = jnp.repeat(attention_mask, 8, axis=0)
+    input_ids = np.repeat(input_ids, 8, axis=0)
+    attention_mask = np.repeat(attention_mask, 8, axis=0)
     print(input_ids.shape)
     print(non_zero)
     print(attention_mask.shape)
-    print(jnp.count_zero(attention_mask))
+    print(np.count_zero(attention_mask))
 
     def sample(input_ids, attention_mask):
         return model.generate(input_ids, attention_mask=attention_mask, do_sample=True)
