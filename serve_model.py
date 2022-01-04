8a13f2b2fe3855f2934b89ceff628932a20ba452
Albert Jiang <qj213@cam.ac.uk>
2022-01-04 17:55:55 +0800

    debug

--- serve_model.py
+++ serve_model.py
@@ -96,6 +96,7 @@ if __name__ == "__main__":
     model = FlaxT5ForConditionalGeneration.from_pretrained(config_path)
     tokenizer = T5TokenizerFast.from_pretrained(config_path)
 
+    
     def sample(input_ids, attention_mask, prng_key):
         return model.generate(input_ids, attention_mask=attention_mask, do_sample=True, prng_key=prng_key)
     fast_generate = pmap(sample)
@@ -106,6 +107,7 @@ if __name__ == "__main__":
     context = "Hello"
     input_ids, attention_mask = tokenize(tokenizer=tokenizer, context=context, n=single_generation_batch, max_source_length=args.max_source_length)
     prng_key = jax.random.PRNGKey(0)
+    _, prng_key = jax.random.split(input_ids.shape[0])
     fast_generate(input_ids, attention_mask, prng_key)
     print(f"Generation compilation done, it took {time.time()-start:.06}s")
 
@@ -138,7 +140,7 @@ if __name__ == "__main__":
         input_ids, attention_mask = tokenize(tokenizer=tokenizer, context=context, n=single_generation_batch, max_source_length=args.max_source_length)
         for i in range(n // single_generation_batch):
             all_tokenized = []
-            _, prng_key = jax.random.split(prng_key)
+            _, prng_key = jax.random.split(input_ids.shape[0])
             outputs = fast_generate(input_ids, attention_mask, prng_key)
             output_ids = outputs.sequences
             output_scores = outputs.scores.squeeze().tolist()
